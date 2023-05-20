b3aa0eb1462ecb826659e9d37d61663de1faba8c
SuneelRathod21 <132656032+SuneelRathod21@users.noreply.github.com>
2023-05-21 00:22:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from script.core.services.Student_services import student_router as apps
 from pymongo import MongoClient
 
 app = FastAPI()
-app.include_router(apps)    #calling the r# outer in the services
+app.include_router(apps)    #calling the router from the services
 
 if __name__ == "__main__":
     uvicorn.run('main:app',host='127.0.0.1', port=8000)
