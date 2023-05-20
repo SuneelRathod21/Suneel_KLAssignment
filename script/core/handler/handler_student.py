a06622c80f6f53cf4cf117c30c6d5f75d86a8ae2
SuneelRathod21 <132656032+SuneelRathod21@users.noreply.github.com>
2023-05-21 00:23:32 +0530

    Update handler_student.py

--- script/core/handler/handler_student.py
+++ script/core/handler/handler_student.py
@@ -1,11 +1,9 @@
 def add_student(self, input_details):
     try:
-        # All Logic here
+       
         print(input_details)
         res = self.mongo_obj.insert_record(input_details)
-        # db = client.interns_b2_23
-        # student_instance = db.Suneel_Rathod
-        # student_instance.insert_one(student.dict())
+       
         if res:
 
             return {"message": "Student added successfully", "status": "success"}
