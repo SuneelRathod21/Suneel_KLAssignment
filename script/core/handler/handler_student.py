317fa77f9fe715356828af063ecc1b06ee090f12
SuneelRathod21 <132656032+SuneelRathod21@users.noreply.github.com>
2023-05-22 18:57:52 +0530

    Update handler_student.py

--- script/core/handler/handler_student.py
+++ script/core/handler/handler_student.py
@@ -1,14 +1,36 @@
-def add_student(self, input_details):
-    try:
-       
-        print(input_details)
-        res = self.mongo_obj.insert_record(input_details)
-       
-        if res:
+from script.utility.mongoDB import Students
+from script.schema import Student
 
-            return {"message": "Student added successfully", "status": "success"}
-        else:
-            return {'message': "", "status": "failed"}
-    except Exception as e:
-        # Log the exception
-        return {'message': "", "status": "failed", "error": str(e)}
+
+def add_student(student: Student):
+    student_dict = student.dict()
+    result = Students.insert_one(student_dict)
+    return {"id": "Student added successfully"}
+
+
+def get_all_students():
+    student = Students.find({})
+    details = []
+    for document in student:
+        detail = {'id': document['id'], 'name': document['name'], 'address': document['address']}
+        details.append(detail)
+    return {"details": details}
+
+
+def update_student(std_id: int, student: Student):
+    result = Students.update_one({"id": std_id}, {"$set": student.dict()})
+    if result.modified_count > 0:
+        return {"message": "Student updated successfully"}
+    else:
+        return {"error": "Student not found"}
+
+
+def delete_student(std_id: int):
+    if std_id in Students.find({}):
+        try:
+            Students.delete_one({"id": std_id})
+            return {"message": "Student deleted successfully"}
+        except Exception as e:
+            return {"error": "Student not found"}
+    else:
+        return {"error": "Nothing to delete"}
